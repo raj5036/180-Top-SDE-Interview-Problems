f2873bc5ba25839d904f0645f3b49f6238986110
Raj Karmakar <97rajmath@gmail.com>
2020-08-18 00:13:34 -0400

    Modified some codes

--- 15.Count trailing zeros in factorial of a number/TrailingZeroes.java
+++ 15.Count trailing zeros in factorial of a number/TrailingZeroes.java
@@ -22,14 +22,14 @@ public class TrailingZeroes{
 		return count;
 	}
 	static int findTrailingZeros(int n) {
-        int count = 0; 
-        // Keep dividing n by powers  
-        // of 5 and update count 
-        for (int i = 5; n / i >= 1; i *= 5) 
-            count += n / i; 
-  
-        return count; 
-    } 
+		int count = 0; 
+		// Keep dividing n by powers  
+		// of 5 and update count 
+		for (int i = 5; n / i >= 1; i *= 5) 
+		    count += n / i; 
+	  
+		return count; 
+    	} 
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
